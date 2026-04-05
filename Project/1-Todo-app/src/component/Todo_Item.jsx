02c966c350ade7bfa0fc321fd2a5db0812c79a8e
bikram <techmarketu9@gmail.com>
2026-04-05 23:00:20 +0530

    apply prettier

--- Project/1-Todo-app/src/component/Todo_Item.jsx
+++ Project/1-Todo-app/src/component/Todo_Item.jsx
@@ -1,20 +1,26 @@
-function Todo_Item({item}) {
-  return (<>
-    {item.map((data)=><div class="row def-row">
-      <div class="col-6">{data.todoName}</div>
-      <div class="col-4">{data.todoDate}</div>
-      <div class="col-2">
-        <button type="button" class="btn btn-danger">Delete</button>
-      </div>
-    </div>)}
-    {/* <div class="row def-row">
+function Todo_Item({ item }) {
+  return (
+    <>
+      {item.map((data) => (
+        <div class="row def-row">
+          <div class="col-6">{data.todoName}</div>
+          <div class="col-4">{data.todoDate}</div>
+          <div class="col-2">
+            <button type="button" class="btn btn-danger">
+              Delete
+            </button>
+          </div>
+        </div>
+      ))}
+      {/* <div class="row def-row">
       <div class="col-6">{item.todoName}</div>
       <div class="col-4">{item.todoDate}</div>
       <div class="col-2">
         <button type="button" class="btn btn-danger">Delete</button>
       </div>
     </div> */}
-  </>);
+    </>
+  );
 }
 
 export default Todo_Item;
